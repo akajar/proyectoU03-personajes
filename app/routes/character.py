eeadf83614a5f5502bf26b9cb5b7d42237d95e68
Bryan Ochoa <bochoatrillo@gmail.com>
2022-11-25 19:18:29 -0500

    Actualiza el archivo de rutas

--- app/routes/character.py
+++ app/routes/character.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from app.models.character import Character
 from app.database import collection
 
 character_route = Blueprint('character_route',__name__)
+
