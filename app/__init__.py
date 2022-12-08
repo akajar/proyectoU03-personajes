a4f23ff693d730cc54813c3ab26517b8e2b8d0d8
Bryan Ochoa <bochoatrillo@gmail.com>
2022-12-08 00:01:23 -0500

    Agrega el blueprint de la ruta de episodios

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from app.routes.character import character_route
+from app.routes.episode import episode_route
 
 def create_app():
     app = Flask(__name__)
     Bootstrap(app)
     app.config.from_object('app.config.Development')
     app.register_blueprint(character_route)
+    app.register_blueprint(episode_route)
     return app
